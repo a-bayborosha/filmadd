1182bb7a4d55d81dd9964f2e9f073b130f38d05d
Anton <anton.bayborosha@gmail.com>
2022-02-20 14:04:48 +0100

    # new functionality

--- script.js
+++ script.js
@@ -1,28 +1,90 @@
-console.log("TEST");
 
-const numberOffFilms = +prompt("How many movies have you already watched?", "");
+
+let numberOffFilms = null;
+
+function start () {
+  do {
+    numberOffFilms = +prompt("How many movies have you already watched?", "");
+ } while (numberOffFilms == "" || numberOffFilms == null || isNaN(numberOffFilms) );
+}
+
+
 
 let personalMovieDB = {
   count: numberOffFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
+  private: false,
 };
 
-//personalMovieDB.movies = { key1: "value1", key2: "value2" };
-let a = null;
-let b = null;
 
-for (let i = 1; i <= 2; i++) {
-  a = prompt("One from the last watched movies?", "");
-  b = prompt("How would you rate it?", "");
-  personalMovieDB.movies[a] = b;
-  a = null;
-  b = null;
+
+function rememberMyFilms() {
+  let a = null;
+  let b = null;
+  for (let i = 1; i <= 2; i++) {
+    a = prompt("One from the last watched movies?"+i, "");
+    b = +prompt("How would you rate it?"+i, "");
+    if (a == "" || a.length > 50 || a == null || b == "" || b == null ) {
+     i--;
+    } else {
+      personalMovieDB.movies[a] = b;
+      a = null;
+      b = null;
+    }
+   
+  }
 }
 
-console.log(personalMovieDB);
-document.getElementById("output").innerHTML = JSON.stringify(
-  personalMovieDB.movies
-);
+
+function writeYouGenres (obj) {
+  let genre;
+  for (let i = 1; i <=3; ) {
+    genre = prompt(`You favorite genre under the number ${i}`, '');
+    if (genre == null || genre == ' ' || !isNaN(genre)) {
+      i === i;
+    } else {
+      obj.genres.push(genre);
+      i++;
+    }
+  }
+ }
+
+
+
+function detectPersonalLevel () {
+    if(personalMovieDB.count >= 0 && personalMovieDB.count < 10 ) {
+      document.getElementById('output').innerHTML = "To few movies watched."
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      document.getElementById('output').innerHTML = "You are a classic viewer."
+  } else if (personalMovieDB.count >= 30) {
+      document.getElementById('output').innerHTML = "You're fucking bad ass."
+  } else {
+      document.getElementById('output').innerHTML = "ERROR!!!."
+  }
+}
+
+detectPersonalLevel();
+
+
+let showMyDB = (obj) => {
+  if (obj.hasOwnProperty('private') && obj.private == false) {
+    console.log(obj);
+  }
+}
+start ();
+rememberMyFilms();
+writeYouGenres(personalMovieDB);
+showMyDB(personalMovieDB);
+
+
+
+
+
+
+ 
+
+
+
+
